6a7b094fe03849f0f87f9fd1fcf3b06145826d49
Micah Conn <mcoxconn@gmail.com>
2021-08-14 15:13:17 -0500

    starting java script pasting in to see outcome

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,46 @@ fetch(queryUrl)
 .then(weatherData => {
     console.log(weatherData);
 })
+
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+
+
+
+
+
+
+
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+  
+    var searchInputVal = document.querySelector('#search-input').value;
+    var formatInputVal = document.querySelector('#format-input').value;
+  
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+  
+    searchApi(searchInputVal, formatInputVal);
+  }
+  
