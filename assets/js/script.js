e88b94e4306ab1024df8ac167e488aa6b00b3134
Micah Conn <mcoxconn@gmail.com>
2021-08-19 14:12:21 -0500

    finally figured out how to get the weatherData that was fetched and appending to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let apiKey = "82177c8d5ce71c99ccc460caecda9690";
 var searchFormEl = document.querySelector('#search-form');
 var resultTextEl = document.querySelector('#result-text')
 var resultContextEl = document.querySelector('#result-content')
-let units = "units=imperial"
+
 
 
 function getParams() {
@@ -14,73 +14,88 @@ function getParams() {
   searchApi(city);
 }
 
-// function printResults(weatherData) {
-//   console.log(weatherData);
+function printResults(weatherData) {
+  console.log(weatherData);
 
-//   var resultCard = document.createElement('div');
-//   resultCard.classList.add('card');
+  // var resultCard = document.createElement('div');
+  // resultCard.classList.add('card');
 
-//   var resultBody = document.createElement('div');
-//   resultBody.classList.add('daily-card');
-//   resultCard.append(resultBody);
-
-//   var titleEl = document.createElement('h2');
-//   titleEl.textContent = weatherData.title;
-
-//   var bodyContentEl = document.createElement('h4');
-//   bodyContentEl.innerHTML =
-//     '<strong>Date:</strong> ' + weatherData.date + '<br/>';
-
-//   if (weatherData.subject) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> ' + weatherData.subject.join(', ') + '<br/>';
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> No subject for this entry.';
-//   }
-
-//   if (weatherData.description) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong> ' + weatherData.description[0];
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong>  No description for this entry.';
-//   }
-
-//   var linkButtonEl = document.createElement('a');
-//   linkButtonEl.textContent = 'Read More';
-//   linkButtonEl.setAttribute('href', weatherData.url);
-//   linkButtonEl.classList.add('btn', 'btn-dark');
   
-//   resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+  // var titleEl = document.createElement('h2');
+  // titleEl.textContent = nameValue;
   
-//   resultContentEl.append(resultCard);
-// }
-
-function searchApi(city) {
-  // var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
-  let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=&cnt=31&units=imperial&appid=${apiKey}`;
+  // var bodyContentEl = document.createElement('h4');
+  // bodyContentEl.innerHTML =
+  //   '<strong>Date:</strong> ' + weatherData.date + '<br/>';
   
-  if (city) {
-    queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=` + city + `&cnt=31&units=imperial&appid=${apiKey}`;
-  
-    fetch(queryUrl)
-    .then(headers => headers.json())
-   
-   
-    .then(weatherData => {
-      console.log(weatherData);
+  // if (weatherData.subject) {
+    //   bodyContentEl.innerHTML +=
+    //     '<strong>Subjects:</strong> ' + weatherData.subject.join(', ') + '<br/>';
+    // } else {
+      //   bodyContentEl.innerHTML +=
+      //     '<strong>Subjects:</strong> No subject for this entry.';
+      // }
       
-      var nameValue = city['name'];
-      var temp = data['main']['temp'];
-      var description = data['main'][0]['description'];
-
-
-
-      location.innerHTML = namevalue;
- 
-        console.log(nameValue)
+      // if (weatherData.description) {
+        //   bodyContentEl.innerHTML +=
+        //     '<strong>Description:</strong> ' + weatherData.description[0];
+        // } else {
+          //   bodyContentEl.innerHTML +=
+          //     '<strong>Description:</strong>  No description for this entry.';
+          // }
+          
+          // var linkButtonEl = document.createElement('a');
+          // linkButtonEl.textContent = 'Read More';
+          // linkButtonEl.setAttribute('href', weatherData.url);
+          // linkButtonEl.classList.add('btn', 'btn-dark');
+          
+          // resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+          
+          // resultContentEl.append(resultCard);
+        }
+        
+        function searchApi(city) {
+          // var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
+          let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=&cnt=31&units=imperial&appid=${apiKey}`;
+          
+          if (city) {
+            queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=` + city + `&cnt=31&units=imperial&appid=${apiKey}`;
+            
+            fetch(queryUrl)
+            .then(headers => headers.json())
+            
+            
+            .then(weatherData => {
+              console.log(weatherData);
+              
+              var nameValue = weatherData.city.name;
+              var temperature = weatherData.list[0].main.temp;
+              var topWind = weatherData.list[0].wind.speed;
+              var humid = weatherData.list[0].main.humidity;
+              
+              var currentCity = document.querySelector('.location');
+              var addCity = document.createTextNode(nameValue);
+              currentCity.appendChild(addCity);
+              
+              var currentTemp = document.querySelector('.current-temp');
+              var addTemp = document.createTextNode(temperature);
+              currentTemp.appendChild(addTemp);
+              
+              var currentWind = document.querySelector('.current-wind');
+              var addWind = document.createTextNode(topWind);
+              currentWind.appendChild(addWind);
+              
+              var currentHumidity = document.querySelector('.current-humidity');
+              var addHumidity = document.createTextNode(humid);
+              currentHumidity.appendChild(addHumidity);
+             
+              
+      console.log(nameValue)
+      console.log(temperature)
+      console.log(topWind)
+      console.log(humid)
       // printResults(weatherData)
+      
     })
   }
 // printResults: function(weatherData) {
